5d97d63139ce918629779fae5b4922768bb72d6e
yongyetan1209 <yongyetan1209@gmail.com>
2025-02-13 22:52:12 -0500

    Initial Commit

---
+++ MoodBoard/MoodBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBoardApp.swift
+//  MoodBoard
+//
+//  Created by Yongye Tan on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
