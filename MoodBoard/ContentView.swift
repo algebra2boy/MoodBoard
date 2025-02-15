34e1507bac814c9322dd8cadeac262c72469895c
yongyetan1209 <yongyetan1209@gmail.com>
2025-02-15 02:55:21 -0500

    bring the sidebarAdaptableback since first responder fixs the issue

--- MoodBoard/ContentView.swift
+++ MoodBoard/ContentView.swift
@@ -8,25 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // a way to persist small and simple data in the disk
+    @AppStorage("sidebarCustomizations") var tabViewCustomization: TabViewCustomization
     
     @State private var selectedTab: AppTabs = .board
     
     @State private var boardViewModel: BoardViewModel = BoardViewModel()
-    
-    var body: some View {
-        
-        TabView(selection: $selectedTab) {
 
-            Tab(AppTabs.board.name, systemImage: AppTabs.board.systemImage, value: .board) {
+    var body: some View {
+
+        TabView(selection: $selectedTab) {
+            
+            Tab(AppTabs.board.name, systemImage: AppTabs.board.systemImage, value: .drawing) {
                 BoardMainLayoutView()
             }
-            
+            .customizationID(AppTabs.board.customizationID)
+
+           
             Tab(AppTabs.drawing.name, systemImage: AppTabs.drawing.systemImage, value: .drawing) {
                 DrawingView()
             }
+            .customizationID(AppTabs.drawing.customizationID)
         }
         .environment(boardViewModel)
-        
+        .tabViewStyle(.sidebarAdaptable)
+        .tabViewCustomization($tabViewCustomization)
+
     }
 }
 
